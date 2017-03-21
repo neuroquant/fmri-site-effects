8db2f0ea223591cae3887ce4267a6dfa060a615a
Manjari Narayan (SU) <mnarayan@users.noreply.github.com>
2017-03-21 16:32:21 -0700

    Removed unnecessary warnings. setup.m adds external packages to the path

--- setup.m
+++ setup.m
@@ -1,8 +1,23 @@
 MATLABLIB = fullfile(getenv('PI_HOME'),'software','matlab','matlab-library');
-addpath(genpath(MATLABLIB)); 
+addpath(genpath(fullfile(MATLABLIB,'lib','ArgUtils'))); 
+addpath(genpath(fullfile(MATLABLIB,'lib','BrewerMap'))); 
+addpath(genpath(fullfile(MATLABLIB,'lib','gramm'))); 
 
 if(exist('ggmClass'))
-	addpath(ggmClass);
-	addpath(ggmClass/external); 
+	warning('off')
+	addpath('ggmClass');
+	addpath('ggmClass/external'); 
 	run('ggmClass/setup.m');
+	warning('on');
+else
+	warning('ggmClass package is missing')
+end
+
+if(exist('connectivity-diagnostics'))
+	addpath('connectivity-diagnostics')
+	if(exist('connectivity-diagnostics/startup.m'))
+		run('connectivity-diagnostics/startup.m');
+	end	
+else
+	warning('connectivity-diagnostics package is missing')
 end
