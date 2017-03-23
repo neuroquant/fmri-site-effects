f569ec4ece8e534f45ea2c669e26fafdd6e15e35
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-03-22 19:35:58 -0700

    setup.m updated to optionally use matlab package manager mpm

--- setup.m
+++ setup.m
@@ -1,15 +1,46 @@
-MATLABLIB = fullfile(getenv('PI_HOME'),'software','matlab','matlab-library');
-if(~exist(MATLABLIB,'dir'))
-	warning('Edit setup.m and set `MATLABLIB` to path to matlab-library');
-	disp('To get matlab-library'); 
-	disp('git clone git@github.com:TheEtkinLab/matlab-library.git'); 
-	disp('Then follow instructions within matlab-library'); 
-end
-addpath(genpath(fullfile(MATLABLIB,'lib','ArgUtils'))); 
-addpath(genpath(fullfile(MATLABLIB,'lib','BrewerMap'))); 
-addpath(genpath(fullfile(MATLABLIB,'lib','gramm'))); 
-addpath(genpath(fullfile(MATLABLIB,'test','matlab-xunit'))); 
+% Use matlab package manager to install requirements.txt
+USE_MPM = false;
 
+% Specify path to matlab-library
+MATLABLIB = fullfile(getenv('PI_HOME'),'software','matlab','matlab-library');
+USE_MATLAB_LIBRARY = true;
+
+if(USE_MPM)
+	if(exist('mpm'))
+		disp('Invoking Matlab Package Manager')
+	else
+		disp('Installing Matlab Package Manager'); 
+		disp('git clone https://github.com/mobeets/mpm.git external/mpm'); 
+	end
+	
+	disp('Installing package dependencies to external/.')
+	PYTHON_EXE = '/usr/bin/python';
+	MPM_INSTALL_DIR = [pwd filesep 'external'];
+	try
+		mpm -r requirements.txt
+		mpm_paths(MPM_INSTALL_DIR);		
+	catch me 
+		disp(me)
+		warning('Could not install packages in requirements.txt. Please install them manually to external/ or add pre-existing packages to your path'); 
+	end
+	MATLABLIB = [pwd filesep 'external'];
+end
+
+if(USE_MATLAB_LIBRARY)	
+	if(~exist(MATLABLIB,'dir'))
+		warning('Edit setup.m and set `MATLABLIB` to path to matlab-library');
+		% disp('To get matlab-library');
+		% disp('git clone git@github.com:TheEtkinLab/matlab-library.git');
+		% disp('Then follow instructions within matlab-library');	
+	else
+		addpath(genpath(fullfile(MATLABLIB,'lib','ArgUtils'))); 
+		addpath(genpath(fullfile(MATLABLIB,'lib','BrewerMap'))); 
+		addpath(genpath(fullfile(MATLABLIB,'lib','gramm'))); 
+		addpath(genpath(fullfile(MATLABLIB,'test','matlab-xunit'))); 
+	end
+end
+
+% Install submodule if missing
 if(exist('ggmClass') & exist('GGM','class'))
 	warning('off')
 	addpath('ggmClass');
@@ -20,16 +51,22 @@ else
 	warning('ggmClass package is missing'); 
 	disp('Attempting to download submodule ...'); 
 	system('git submodule update --init ggmClass'); 
-	warning('off')
+	warning('off');
 	run('ggmClass/setup.m');
 	warning('on');
 end
 
-if(exist('connectivity-diagnostics'))
+% Install submodule if missing
+if(exist('connectivity-diagnostics') & exist('connectivity-diagnostics/startup.m','file'))
 	addpath('connectivity-diagnostics')
 	if(exist('connectivity-diagnostics/startup.m'))
 		run('connectivity-diagnostics/startup.m');
 	end	
 else
 	warning('connectivity-diagnostics package is missing')
+	disp('Attempting to download submodule ...'); 
+	system('git submodule update --init connectivity-diagnostics'); 
+	warning('off'); 
+	run('connectivity-diagnostics/startup.m');
+	warning('on');
 end
