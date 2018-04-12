d82d03c0422104b90a8b5913358e35b0c6ab0029
mnarayan <mnarayan@users.noreply.github.com>
2018-04-12 11:45:59 -0700

    Added conditional for using ranksum or mwwTest

--- compareWithinAndBetweenGroupsSim.m
+++ compareWithinAndBetweenGroupsSim.m
@@ -15,40 +15,43 @@ function [ hom sep mw ] = compareWithinAndBetweenGroupsSim( simMat, grouping )
 % 
 % 2017, Adi Maron-Katz
 
-withinSimVals=[];
-betweenSimVals=[];
-numElems=size(simMat,1);
-for i=1:(numElems-1)
-    for j=(i+1):numElems
-        if(grouping(i)>0 && grouping(i)==grouping(j))
-            withinSimVals=[withinSimVals,simMat(i,j)];
-        else
-            betweenSimVals=[betweenSimVals,simMat(i,j)];
+    withinSimVals=[];
+    betweenSimVals=[];
+    numElems=size(simMat,1);
+    for i=1:(numElems-1)
+        for j=(i+1):numElems
+            if(grouping(i)>0 && grouping(i)==grouping(j))
+                withinSimVals=[withinSimVals,simMat(i,j)];
+            else
+                betweenSimVals=[betweenSimVals,simMat(i,j)];
+            end
         end
     end
-end
-hom=mean(withinSimVals);
-sep=mean(betweenSimVals);
+    hom=mean(withinSimVals);
+    sep=mean(betweenSimVals);
 
-
- % [mw.p,mw.h,mw.stats] =
- % ranksum(withinSimVals,betweenSimVals,'tail','right');%This function is
- % The above two lines have been replaced with the code below for Octave compatibility:
- mwRes=mwwtest(betweenSimVals,withinSimVals);
- direction=sign(mean(withinSimVals)-mean(betweenSimVals));
- mw.stats.zval=direction*mwRes.Z;
- if(direction>0)
-     mw.p=mwRes.p(1);
- else
-     mw.p=1-mwRes.p(1);
- end
- if(mw.p<0.05)
-     mw.h=1;
- else
-     mw.h=0;
- end
- mw.stats.ranksum=mwRes.W(1);
+    if(exist('ranksum'))
+        [mw.p,mw.h,mw.stats] = ...
+             ranksum(withinSimVals,betweenSimVals,'tail','right');
+    else
+        % Use below for Octave compatibility
+        mwRes=mwwtest(betweenSimVals,withinSimVals);
+        direction=sign(mean(withinSimVals)-mean(betweenSimVals));
+        mw.stats.zval=direction*mwRes.Z;
+        if(direction>0)
+         mw.p=mwRes.p(1);
+        else
+         mw.p=1-mwRes.p(1);
+        end
+        if(mw.p<0.05)
+         mw.h=1;
+        else
+         mw.h=0;
+        end
+    end
+    mw.stats.ranksum=mwRes.W(1);
 end
+
  %[mw.p, mw.stats.zval] = u_test (withinSimVals,betweenSimVals, '<');
  %if(mw.p<0.05)
  %    mw.h=1;
